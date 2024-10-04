34c903cb99d9f5770b87d04c7bcaf8129b567c41
lanaoliveira2020 <144233328+lanaoliveira2020@users.noreply.github.com>
2024-10-03 23:32:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Culinária',
+    'Qual o melhor prato tipíco brasieiro?',
+    'O melhor prato brasilero é a feijoada'
+)
+
+criaCartao(
+    'Culinária',
+    'bolo da vovó?',
+    'Bolo de fubá com goiabada'
+)
+
+criaCartao(
+    'Culinária',
+    'Ingredientes para fazer pão de queijo',
+    '......................'
+)
+
+criaCartao(
+    'Culinária',
+    'Quais ingredientes usa-se na paçoca?',
+    'leite condensado e amendoim'
+)
+
+criaCartao(
+    'Culinária',
+    'Doce tipíco da região brasileira?',
+    'brigadeiroooooo'
+)
+
+criaCartao(
+    'Culinária',
+    'Bebida tipíca da região brasileira',
+    '......................'
+)
